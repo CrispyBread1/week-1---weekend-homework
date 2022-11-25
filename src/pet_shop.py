34ab2f80a2e23e12fdb2cb179c393ceba271f92e
Daniel <dsalvatori2008@gmail.com>
2022-11-25 11:46:23 +0000

    Second 2 done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,3 +4,8 @@ def get_pet_shop_name(pet_shop):
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, num1):
+    total_money = pet_shop["admin"]["total_cash"]
+    pet_shop["admin"]["total_cash"] = int(total_money) + int(num1)
+    
