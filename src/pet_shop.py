ba357320c456e2e25738e7f7745c4cb1421d66e3
Daniel <dsalvatori2008@gmail.com>
2022-11-25 14:14:39 +0000

    done some more but struggling

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,11 +1,41 @@
 # WRITE YOUR FUNCTIONS HERE
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
-
+# 1
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
-
+# 2
 def add_or_remove_cash(pet_shop, num1):
     total_money = pet_shop["admin"]["total_cash"]
     pet_shop["admin"]["total_cash"] = int(total_money) + int(num1)
-    
+# 3
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+# 4
+def increase_pets_sold(pet_shop, num1):
+    pet_shop["admin"]["pets_sold"] = num1
+    return pet_shop["admin"]["pets_sold"]
+# 5
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+# 6
+def get_pets_by_breed(pet_shop, pet_breed):
+    pets_breed = []
+    for pets in pet_shop["pets"]:
+        if pets["breed"] == pet_breed:
+            pets_breed.append('pets') 
+    return pets_breed
+# 7
+def find_pet_by_name(pet_shop, pet_name):
+    for pets in pet_shop["pets"]:
+        if pets["name"] == pet_name:
+            pet_name = pets
+            return pet_name
+# 8
+def remove_pet_by_name(pet_shop, name):
+    number_in_list = 0
+    while pet_shop["pets"] != name:
+        number_in_list += 1
+        if pet_shop['pets'] == name:
+            break
+    pet_shop["pets"].pop(number_in_list)
--- tests/pet_shop_test.py
+++ tests/pet_shop_test.py
@@ -96,43 +96,43 @@ class TestPetShop(unittest.TestCase):
     #     cash = get_total_cash(self.cc_pet_shop)
     #     self.assertEqual(990, cash)
 
-    @unittest.skip("delete this line to run the test")
-    def test_pets_sold(self):
-        sold = get_pets_sold(self.cc_pet_shop)
-        self.assertEqual(0, sold)
+    
+    # def test_pets_sold(self):
+    #     sold = get_pets_sold(self.cc_pet_shop)
+    #     self.assertEqual(0, sold)
 
-    @unittest.skip("delete this line to run the test")
-    def test_increase_pets_sold(self):
-        increase_pets_sold(self.cc_pet_shop,2)
-        sold = get_pets_sold(self.cc_pet_shop)
-        self.assertEqual(2, sold)
+    
+    # def test_increase_pets_sold(self):
+    #     increase_pets_sold(self.cc_pet_shop,2)
+    #     sold = get_pets_sold(self.cc_pet_shop)
+    #     self.assertEqual(2, sold)
 
-    @unittest.skip("delete this line to run the test")
-    def test_stock_count(self):
-        count = get_stock_count(self.cc_pet_shop)
-        self.assertEqual(6, count)
+    
+    # def test_stock_count(self):
+#     #     count = get_stock_count(self.cc_pet_shop)
+#     #     self.assertEqual(6, count)
 
-    @unittest.skip("delete this line to run the test")
-    def test_all_pets_by_breed__found(self):
-        pets = get_pets_by_breed(self.cc_pet_shop, "British Shorthair")
-        self.assertEqual(2, len(pets))
+# #    did not finish this one it sucked!!!!
+#     def test_all_pets_by_breed__found(self):
+#         pets = get_pets_by_breed(self.cc_pet_shop, "British Shorthair") 
+#         self.assertEqual(2, len(pets))
 
-    @unittest.skip("delete this line to run the test")
-    def test_all_pets_by_breed__not_found(self):
-        pets = get_pets_by_breed(self.cc_pet_shop, "Dalmation")
-        self.assertEqual(0, len(pets))
+    
+    # def test_all_pets_by_breed__not_found(self):
+    #     pets = get_pets_by_breed(self.cc_pet_shop, "Dalmation")
+    #     self.assertEqual(0, len(pets))
 
-    @unittest.skip("delete this line to run the test")
-    def test_find_pet_by_name__returns_pet(self):
-        pet = find_pet_by_name(self.cc_pet_shop, "Arthur")
-        self.assertEqual("Arthur", pet["name"])
+    
+    # def test_find_pet_by_name__returns_pet(self):
+    #     pet = find_pet_by_name(self.cc_pet_shop, "Arthur")
+    #     self.assertEqual("Arthur", pet["name"])
 
-    @unittest.skip("delete this line to run the test")
-    def test_find_pet_by_name__returns_None(self):
-        pet = find_pet_by_name(self.cc_pet_shop, "Fred")
-        self.assertIsNone(pet)
+   
+    # def test_find_pet_by_name__returns_None(self):
+    #     pet = find_pet_by_name(self.cc_pet_shop, "Fred")
+    #     self.assertIsNone(pet)
 
-    @unittest.skip("delete this line to run the test")
+    
     def test_remove_pet_by_name(self):
         remove_pet_by_name(self.cc_pet_shop, "Arthur")
         pet = find_pet_by_name(self.cc_pet_shop,"Arthur")
